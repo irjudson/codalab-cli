2afb911f319823626c13d077154a72e79b15219a
Ivan R. Judson <irjudson@gmail.com>
2014-04-01 08:45:37 -0700

    @cpoulain - I believe this addresses the bulk of your comments...the getppid issue is still unresolved, but functions. I'll take an inventory of the remaining commands that need to be ported.

--- codalab/bin/cl.bat
+++ codalab/bin/cl.bat
@@ -4,6 +4,16 @@ REM TODO: replace this with an nicer unified Python interface that unifies the
 REM server and client.
 REM Once you install your virtual environment, activate it and 'pip install .'
 REM 
-set CODALAB="C:\dev\codalab-cli"
+
+REM Twisted way of getting the directory two levels above us in windows command prompt
+pushd %~dp0
+   pushd .. 
+      pushd .. 
+         set CODALAB=%cd%
+      popd 
+    popd 
+popd
+
+set PYTHONPATH="%PYTHONPATH%;%CODALAB%"
 
 python %CODALAB%\codalab\bin\codalab_client.py %CODALAB%\codalab\config\sqlite_client_config.json "%*"
